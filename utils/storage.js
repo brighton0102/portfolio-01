70647f03bcf32eb533357ef17d109a01a8f59a5d
brighton0102 <mtandibrighton0102@gmail.com>
2023-07-27 18:28:44 +0200

    Modified save data to local storage function

--- utils/storage.js
+++ utils/storage.js
@@ -1,12 +1,20 @@
-const names = document.querySelector('#user');
-const email = document.querySelector('#email');
-const message = document.querySelector('#comment');
 
-form.addEventListener('keyup', () => {
-    const formData = {
-      names: names.value,
-      email: email.value,
-      message: message.value,
-    };
+window.addEventListener('DOMContentLoaded', () => {
+    const formData = localStorage.getItem('formData');
+    if (formData) {
+      const { name, email, message } = JSON.parse(formData);
+      document.getElementById('user').value = name || '';
+      document.getElementById('email').value = email || '';
+      document.getElementById('comment').value = message || '';
+    }
+  });
+  
+  // Save form data to local storage whenever the input fields change
+  
+  document.getElementById('contact-form').addEventListener('input', () => {
+    const name = document.getElementById('user').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('comment').value;
+    const formData = { name, email, message };
     localStorage.setItem('formData', JSON.stringify(formData));
   });
