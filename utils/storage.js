fa6d0a55301ad00dcf0d8e520abfd6393607698b
Jery Mey <jerymey25@gmail.com>
2023-07-27 09:25:13 +0100

    Add even Listener to the local storage

--- utils/storage.js
+++ utils/storage.js
@@ -1,3 +1,12 @@
 const names = document.querySelector('#user');
 const email = document.querySelector('#email');
 const message = document.querySelector('#comment');
+
+form.addEventListener('keyup', () => {
+    const formData = {
+      names: names.value,
+      email: email.value,
+      message: message.value,
+    };
+    localStorage.setItem('formData', JSON.stringify(formData));
+  });
