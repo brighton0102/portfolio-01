5511a9870d812123766e488a45b1c040da2ea6a5
brighton0102 <mtandibrighton0102@gmail.com>
2023-07-27 18:32:27 +0200

    Fix linter error

--- utils/storage.js
+++ utils/storage.js
@@ -1,20 +1,19 @@
-
 window.addEventListener('DOMContentLoaded', () => {
-    const formData = localStorage.getItem('formData');
-    if (formData) {
-      const { name, email, message } = JSON.parse(formData);
-      document.getElementById('user').value = name || '';
-      document.getElementById('email').value = email || '';
-      document.getElementById('comment').value = message || '';
-    }
-  });
-  
-  // Save form data to local storage whenever the input fields change
-  
-  document.getElementById('contact-form').addEventListener('input', () => {
-    const name = document.getElementById('user').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('comment').value;
-    const formData = { name, email, message };
-    localStorage.setItem('formData', JSON.stringify(formData));
-  });
+  const formData = localStorage.getItem('formData');
+  if (formData) {
+    const { name, email, message } = JSON.parse(formData);
+    document.getElementById('user').value = name || '';
+    document.getElementById('email').value = email || '';
+    document.getElementById('comment').value = message || '';
+  }
+});
+
+// Save form data to local storage whenever the input fields change
+
+document.getElementById('contact-form').addEventListener('input', () => {
+  const name = document.getElementById('user').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('comment').value;
+  const formData = { name, email, message };
+  localStorage.setItem('formData', JSON.stringify(formData));
+});
